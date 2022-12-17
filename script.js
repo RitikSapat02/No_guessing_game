3b63ad842a9de76b5c4e925ccd39701808a865e8
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2022-12-17 14:58:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+// console.log(displayMessage(
+// displayMess)age( 'Correct Numbe)r!';
+// console.log(displayMessage(
+
+// document.q)uerySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 22;
+// console.log(document.querySelector('.guess').value);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    displayMessage('😒No Number!');
+  } else if (guess == secretNumber) {
+    displayMessage('🏆Correct Number!!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(
+        guess > secretNumber ? 'Lower Number plz!' : 'Higher Number plz'
+      );
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('😢😢You lossed the game!!');
+      document.querySelector('.score').textContent = 0;
+    }
+    document.querySelector('.score').textContent = score;
+  }
+
+  document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20 + 1);
+    displayMessage('start guessing....');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+  });
+});
